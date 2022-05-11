f644c30e5e13174b11a54bbe1c621b2d0bd312fe
Karim Radhouani <medkarimrdi@gmail.com>
2022-05-11 13:41:26 -0700

    fix install script

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@
 : ${REPO_NAME:="karimra/gribic"}
 : ${REPO_URL:="https://github.com/$REPO_NAME"}
 : ${PROJECT_URL:="https://gribic.kmrd.dev"}
-
+: ${LATEST_URL:="https://api.github.com/repos/$REPO_NAME/releases/latest"}
 # detectArch discovers the architecture for this system.
 detectArch() {
     ARCH=$(uname -m)
@@ -80,23 +80,37 @@ verifyOpenssl() {
     fi
 }
 
+
 # setDesiredVersion sets the desired version either to an explicit version provided by a user
 # or to the latest release available on github releases
 setDesiredVersion() {
     if [ "x$DESIRED_VERSION" == "x" ]; then
         # when desired version is not provided
         # get latest tag from the gh releases
+        local cmd=""
         if type "curl" &>/dev/null; then
-            local latest_release_url=$(curl -s $REPO_URL/releases/latest | cut -d '"' -f 2)
-            TAG=$(echo $latest_release_url | cut -d '"' -f 2 | awk -F "/" '{print $NF}')
-            # tag with stripped `v` prefix
-            TAG_WO_VER=$(echo "${TAG}" | cut -c 2-)
+            cmd="curl -s "
         elif type "wget" &>/dev/null; then
-            # get latest release info and get 5th line out of the response to get the URL
-            local latest_release_url=$(wget -q https://api.github.com/repos/$REPO_NAME/releases/latest -O- | sed '5q;d' | cut -d '"' -f 4)
-            TAG=$(echo $latest_release_url | cut -d '"' -f 2 | awk -F "/" '{print $NF}')
-            TAG_WO_VER=$(echo "${TAG}" | cut -c 2-)
+            cmd="wget -q -O- "
+        else
+            echo "Missing curl or wget utility to download the installation package"
+            exit 1
         fi
+        local latest_release_url=""
+        # use jq to filter the api response if available
+        if type "jq" &>/dev/null; then
+            latest_release_url=$($cmd $LATEST_URL | jq -r .html_url)
+        # else use grep and cut
+        else
+            latest_release_url=$($cmd $LATEST_URL | grep "html_url.*releases/tag" | cut -d '"' -f 4)
+        fi
+        if [ "x$latest_release_url" == "x" ]; then
+            echo "Could not determine the latest release"
+            exit 1
+        fi
+        TAG=$(echo $latest_release_url | cut -d '"' -f 2 | awk -F "/" '{print $NF}')
+        # tag with stripped `v` prefix
+        TAG_WO_VER=$(echo "${TAG}" | cut -c 2-)
     else
         TAG=$DESIRED_VERSION
         TAG_WO_VER=$(echo "${TAG}" | cut -c 2-)
